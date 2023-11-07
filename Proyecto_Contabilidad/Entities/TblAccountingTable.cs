834c1ea0f7fa6bee3c3f0eb452c56f5238c6dfb5
Nayarit <nayarit2904@gmail.com>
2023-11-07 17:23:54 -0600

    new

---
+++ Proyecto_Contabilidad/Entities/TblAccountingTable.cs
@@ -0,0 +1,13 @@
+﻿namespace Proyecto_Contabilidad.Entities
+{
+    public class TblAccountingTable
+    {
+        public int ID { get; set; }
+        public decimal MONTO { get; set; }
+        public string Currency { get; set; }
+        public int ID_CUENTA { get; set; }
+        public string DESCRIPCION { get; set; }
+        public int ID_ASIENTO { get; set; }
+    }
+
+}
